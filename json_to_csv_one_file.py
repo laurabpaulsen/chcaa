054d4630a274bfcd6726b33dbb370ee44e6a4bc3
laurabpaulsen <90269980+laurabpaulsen@users.noreply.github.com>
2022-01-04 17:39:45 +0100

    Update json_to_csv_one_file.py

--- json_to_csv_one_file.py
+++ json_to_csv_one_file.py
@@ -80,6 +80,6 @@ if __name__ == '__main__':
 
     df = load_data(args['filepath'])
     df["category"] = df["mentioner"].apply(lambda x:get_category(x, media_list, diplomat_list))
-    df.to_csv('mentiondata/%s.csv' % args['filepath'].split("/"))
+    df.to_csv('mentiondata/%s.csv' % args['filepath'].split("/")[-1])
 
 
