8297284d791b797f84f463876226174b8473840e
laurabpaulsen <90269980+laurabpaulsen@users.noreply.github.com>
2022-01-05 13:44:07 +0100

    first of the list

--- json_to_csv_one_file.py
+++ json_to_csv_one_file.py
@@ -53,7 +53,7 @@ def convert_to_df(data):
     dataframe = {
         "mentioner": [row["includes"]["users"][0]["username"] for row in data],
         "mentionee": [get_mentionee(row['entities'].get('mentions')) if row.get("entities") else '' for row in data],
-        "text": [[row['text'] for row in row.get('includes')['tweets']] if row["text"].encode("utf-8").startswith("RT @") else row["text"].encode("utf-8") for row in data]
+        "text": [[row['text'] for row in row.get('includes')['tweets']][0] if row["text"].encode("utf-8").startswith("RT @") else row["text"].encode("utf-8") for row in data]
         }
     return pd.DataFrame(dataframe)
 
