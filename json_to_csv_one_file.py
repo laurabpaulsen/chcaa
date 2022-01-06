11440b110438cd605c236bde6b6c909e69b39d00
laurabpaulsen <90269980+laurabpaulsen@users.noreply.github.com>
2022-01-06 12:36:25 +0100

    Update json_to_csv_one_file.py

--- json_to_csv_one_file.py
+++ json_to_csv_one_file.py
@@ -42,7 +42,7 @@ def get_mentionee1(tweet_data):
     if tweet_data.get('includes')['tweets'][0]:
         tweetinfo = tweet_data.get('includes')['tweets'][0]
     
-    if tweetinfo.get('entities').get('mentions', 'False'):
+    if tweetinfo.get('entities').get('mentions'):
         fullmentioninfo = tweetinfo['entities']['mentions']
         list_of_mentions = [tweet['username'] for tweet in fullmentioninfo]
     
