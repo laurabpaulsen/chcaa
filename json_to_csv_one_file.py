8b4c958af80bdf85c087231cea80229aa4c895f6
laurabpaulsen <90269980+laurabpaulsen@users.noreply.github.com>
2022-01-19 10:17:06 +0100

    Update json_to_csv_one_file.py

--- json_to_csv_one_file.py
+++ json_to_csv_one_file.py
@@ -21,6 +21,8 @@ media_list = ["ouzhounews", "shen_shiwei", "CGTNOfficial", "XHNews", "ChinaDaily
 
 diplomat_list = ['AmbassadeChine', 'Amb_ChenXu', 'ambcina', 'AmbCuiTiankai', 'AmbLiuXiaoMing','CCGBelfast','ChinaAmbUN','chinacgedi', 'ChinaConsulate', 'ChinaEmbassyUSA','ChinaEmbGermany','ChinaEUMission','ChinaInDenmark','China_Lyon','Chinamission2un','ChinaMissionGva','ChinaMissionVie','chinascio', 'ChineseEmbinUK', 'ChineseEmbinUS', 'ChnMission','CHN_UN_NY', 'consulat_de', 'EUMissionChina','GeneralkonsulDu','MFA_China','SpokespersonCHN', 'spokespersonHZM','zlj517', 'AmbCina']
 
+handle = list(args['filepath'].split('mention_')[-1].split('_2007-01')[0])
+
 def get_mentionee(mention_info):
     ''' Extracts mention info from tweets
     Args: 
@@ -30,30 +32,8 @@ def get_mentionee(mention_info):
     '''
     if not mention_info:
         return ''
-    return ",".join(info['username'] for info in mention_info if info['username'] in diplomat_list)
+    return ",".join(info['username'] for info in mention_info if info['username'] in handle)
 
-def get_mentionee1(tweet_data):
-    ''' Extracts urls from tweets
-    Args: 
-        Media info from tweet object (list of dicts or 0)
-    Returns:
-        Urls if there is some
-    '''
-    list_of_mentions = []
-    
-    if tweet_data.get('includes'):
-        tweetinfo = tweet_data.get('includes')
-        if tweetinfo.get('tweets'):
-            tweetinfo = tweetinfo['tweets'][0]
-            if tweetinfo.get('entities'):
-                tweetinfo = tweetinfo.get('entities')
-                if tweetinfo.get('mentions'):
-                    fullmentioninfo = tweetinfo['mentions']
-                    list_of_mentions = [tweet['username'] for tweet in fullmentioninfo]
-    
-    if not list_of_mentions:
-        return ''
-    return ",".join(info for info in list_of_mentions if info in diplomat_list)
 
 def get_category(string, media_list, diplomat_list):
     if string in media_list:
@@ -86,7 +66,7 @@ def convert_to_df(data):
     dataframe = {
         "tweetID": [row.get('id').encode("utf-8") for row in data],
         "mentioner": [row["includes"]["users"][0]["username"] for row in data],
-        "mentionee": [get_mentionee1(row) if row["text"].encode("utf-8").startswith("RT @") else get_mentionee(row['entities'].get('mentions')) for row in data],
+        "mentionee": [get_mentionee(row) for row in data],
         "text": [[row['text'] for row in row.get('includes')['tweets']][0] if check(row) else row["text"].encode("utf-8") for row in data],
         "retweet": [row["referenced_tweets"][0]["type"] if row.get("referenced_tweets") else "original" for row in data]
         }
@@ -114,16 +94,10 @@ if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('-f','--filepath', required=True, help='path to dir with json')
     args = vars(ap.parse_args())
-    handle = args['filepath'].split('mention_')[-1].split('_2007-01')[0]
 
     df = load_data(args['filepath'])
     df["category"] = df["mentioner"].apply(lambda x:get_category(x, media_list, diplomat_list))
+    df["category_mentionee"] = df["mentionee"].apply(lambda x:get_category(x, media_list, diplomat_list))
     df['mentionee'] = df['mentionee'].replace('', handle)
-    lst_col = 'mentionee' 
-    x = df.assign(**{lst_col:df[lst_col].str.split(',')})
-    df = pd.DataFrame({
-        col:np.repeat(x[col].values, x[lst_col].str.len())
-        for col in x.columns.difference([lst_col])
-    }).assign(**{lst_col:np.concatenate(x[lst_col].values)})[x.columns.tolist()]
     df['tweetID']= df['tweetID'].astype('int')
     df.to_csv('mentiondata/%s.csv' % handle, index = False, encoding =  "utf-8")
